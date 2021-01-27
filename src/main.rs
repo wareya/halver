478690408dc21500037a1c98647075e0549ce0e5
wareya <wareya@gmail.com>
2021-01-27 05:04:40 -0500

    attempt 3

--- src/main.rs
+++ src/main.rs
@@ -60,6 +60,140 @@ halver <infile> <outfile>
         out
     };
     
+    let median_tile = |tx : u32, ty : u32|
+    {
+        let mut out = image::RgbaImage::new(out_size, out_size);
+        let in_x  = tx * in_size;
+        let in_y  = ty * in_size;
+        let out_x = tx * out_size;
+        let out_y = ty * out_size;
+        for iy in 0..out_size
+        {
+            for ix in 0..out_size
+            {
+                let mut px = Vec::new();
+                let mut push = |px : &mut Vec<_>, x : i64, y : i64|
+                {
+                    let mut x = in_x as i64 + ix as i64*2 + x;
+                    let mut y = in_y as i64 + iy as i64*2 + y;
+                    //if x < in_x as i64 || x >= (in_x + in_size) as i64 || y < in_y as i64 || y >= (in_y + in_size) as i64
+                    //{
+                    //    return;
+                    //}
+                    if x < in_x as i64
+                    {
+                        x += in_size as i64;
+                    }
+                    if x >= (in_x + in_size) as i64
+                    {
+                        x -= in_size as i64;
+                    }
+                    if y < in_y as i64
+                    {
+                        y += in_size as i64;
+                    }
+                    if y >= (in_y + in_size) as i64
+                    {
+                        y -= in_size as i64;
+                    }
+                    let pixel = in_img.borrow().get_pixel(x.try_into().unwrap(), y.try_into().unwrap()).to_rgba();
+                    if pixel.channels()[3] < 127 { return; }
+                    px.push(pixel);
+                };
+                for x in -1..=2
+                {
+                    for y in -1..=2
+                    {
+                        push(&mut px, x, y);
+                    }
+                }
+                if px.len() == 0
+                {
+                    px.push(in_img.borrow().get_pixel(in_x+ix*2, in_y+iy*2).to_rgba());
+                }
+                
+                let luma = |rgba : &image::Rgba<u8>| rgba.to_luma().channels()[0];
+                px.sort_by_key(luma);
+                let mut px_luma = px.iter().map(|x| luma(x) as f32).collect::<Vec<_>>();
+                
+                let mid_luma = 
+                if px_luma.len() % 2 == 1
+                {
+                    px_luma[px_luma.len()/2]
+                }
+                else
+                {
+                    (px_luma[px_luma.len()/2] + px_luma[px_luma.len()/2-1])/2.0
+                };
+                
+                let mut polarity = 0;
+                if (px_luma[0] - mid_luma).abs() > (px_luma[px.len()-1] - mid_luma).abs()
+                {
+                    polarity = 1;
+                }
+                else if (px_luma[0] - mid_luma).abs() < (px_luma[px.len()-1] - mid_luma).abs()
+                {
+                    polarity = -1;
+                }
+                
+                px = Vec::new();
+                for x in 0..=1
+                {
+                    for y in 0..=1
+                    {
+                        push(&mut px, x, y);
+                    }
+                }
+                if px.len() == 0
+                {
+                    px.push(in_img.borrow().get_pixel(in_x+ix*2, in_y+iy*2).to_rgba());
+                }
+                
+                px.sort_by_key(luma);
+                let mut px_luma = px.iter().map(|x| luma(x) as f32).collect::<Vec<_>>();
+                
+                
+                let out_px;
+                if polarity == 0
+                {
+                    out_px = 
+                    if px.len() % 2 == 1
+                    {
+                        px[px.len()/2]
+                    }
+                    else
+                    {
+                        let a = px[px.len()/2].channels();
+                        let b = px[px.len()/2-1].channels();
+                        image::Rgba::<u8>::from_channels(
+                            ((a[0] as u32+b[0] as u32)/2) as u8,
+                            ((a[1] as u32+b[1] as u32)/2) as u8,
+                            ((a[2] as u32+b[2] as u32)/2) as u8,
+                            ((a[3] as u32+b[3] as u32)/2) as u8)
+                    };
+                }
+                else
+                {
+                    let max = px.len() as f32 - 1.0;
+                    let mut mid = 0.5;
+                    if polarity == 1
+                    {
+                        mid -= 0.5;
+                    }
+                    else
+                    {
+                        mid += 0.5;
+                    }
+                    let position = mid*max;
+                    out_px = px[position as usize];
+                }
+                
+                out.put_pixel(ix, iy, out_px);
+            }
+        }
+        out
+    };
+    
     let write_tile = |tx, ty, tile : &image::RgbaImage|
     {
         for iy in 0..out_size
@@ -93,7 +227,7 @@ halver <infile> <outfile>
         {
             for ix in 0..size
             {
-                let px = luma(&img.get_pixel(ix, iy).to_rgba());
+                let px = luma(&img.get_pixel(ix, iy).to_rgba()) - avg;
                 rms += px * px;
             }
         }
@@ -101,7 +235,7 @@ halver <infile> <outfile>
         (avg, rms)
     };
     
-    let dist2 = |x1 : f32, y1 : f32, x2 : f32, y2 : f32| OrderedFloat::from((x1-x2).powi(2) + (y1-y2).powi(2));
+    let dist2 = |a : (f32, f32), b : (f32, f32)| OrderedFloat::from((a.0-b.0).powf(2.0) + (a.1-b.1).powf(2.0));
     
     for iy in 0..in_dim.1/in_size
     {
@@ -112,14 +246,15 @@ halver <infile> <outfile>
             tiles.push(downscale_tile(ix, iy, (0, 1)));
             tiles.push(downscale_tile(ix, iy, (1, 0)));
             tiles.push(downscale_tile(ix, iy, (1, 1)));
+            tiles.push(median_tile(ix, iy));
             
             let m = measure_tile(ix, iy, in_size, &in_img.borrow());
             tiles.sort_by_key(|tile|
             {
                 let m2 = measure_tile(ix, iy, out_size, tile);
-                dist2(m.0, m.1, m2.0, m2.1)
+                dist2(m, m2)
             });
-            write_tile(ix, iy, &tiles[3]);
+            write_tile(ix, iy, &tiles[0]);
         }
     }
     
